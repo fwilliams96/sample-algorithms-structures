fe971b432e465bfd64b8c77e4cd3cc908255442e
Francisco <fwmcomputer@gmail.com>
2023-05-24 20:03:20 +0200

    First commit with the skeleton

---
+++ src/main/java/com/williams/samplealgorithmsstructures/SampleAlgorithmsStructuresApplication.java
@@ -0,0 +1,13 @@
+package com.williams.samplealgorithmsstructures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAlgorithmsStructuresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAlgorithmsStructuresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/williams/samplealgorithmsstructures/SampleAlgorithmsStructuresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williams.samplealgorithmsstructures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAlgorithmsStructuresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
