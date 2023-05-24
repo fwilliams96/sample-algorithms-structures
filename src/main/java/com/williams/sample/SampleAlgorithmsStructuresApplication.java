a568e5cfa4bee2ed1bd6afff31d311b03b7b6753
Francisco <fwmcomputer@gmail.com>
2023-05-24 20:34:59 +0200

    Add modules algorithms and structures

--- src/main/java/com/williams/sample/SampleAlgorithmsStructuresApplication.java
+++ src/main/java/com/williams/sample/SampleAlgorithmsStructuresApplication.java
@@ -1,4 +1,4 @@
-package com.williams.samplealgorithmsstructures;
+package com.williams.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SampleAlgorithmsStructuresApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello world");
 		SpringApplication.run(SampleAlgorithmsStructuresApplication.class, args);
 	}
 
