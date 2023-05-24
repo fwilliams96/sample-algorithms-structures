9d6b5f7928e4221788999f380a0cfad1c8b0dd14
Francisco <fwmcomputer@gmail.com>
2023-05-24 21:22:33 +0200

    Create all the modules

---
+++ src/main/java/com/williams/sample.orders_distribution/SampleOrdersDistributionApplication.java
@@ -0,0 +1,13 @@
+package com.williams.sample.orders_distribution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleOrdersDistributionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleOrdersDistributionApplication.class, args);
+	}
+
+}
